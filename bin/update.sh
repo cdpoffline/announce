7a3276cdca55ad9177e4a659da455929486e093a
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-02-03 18:09:36 +0100

    removed starting bug

--- bin/update.sh
+++ bin/update.sh
@@ -2,13 +2,15 @@
 
 cd "`dirname \"$0\"`"
 
+echo ---------- deactivate ----------
 ./deactivate.sh
 
+echo ---------- activate ----------
 # this MUST be the same in deactivate.sh
 comment="# announce the offline server"
-rc_local_line="sudo -u \"`pwd`\"/announce.sh $comment"
+rc_local_line="sudo -u `whoami` \"`pwd`\"/start_announce.sh $comment"
 escaped_rc_local_line="`echo \"$rc_local_line\" | sed -e 's/[\\/\\\\\\&]/\\\\&/g'`"
-echo "command: \"$new_text\""
+echo "command: \"$rc_local_line\""
 echo "escaped line in /etc/rc.local: \"$escaped_rc_local_line\""
 sudo sed -i "s/^exit 0/$escaped_rc_local_line\nexit 0/g" /etc/rc.local
 
