e762ca51bc311abfeeb794d416f1710ea05996f7
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-03-10 02:12:12 -0800

    using the startup module

--- bin/update.sh
+++ bin/update.sh
@@ -2,17 +2,8 @@
 
 cd "`dirname \"$0\"`"
 
-echo ---------- deactivate ----------
-./deactivate.sh
-
-echo ---------- activate ----------
-# this MUST be the same in deactivate.sh
-comment="# announce the offline server"
-rc_local_line="sudo -u `whoami` \"`pwd`\"/start_announce.sh $comment"
-escaped_rc_local_line="`echo \"$rc_local_line\" | sed -e 's/[\\/\\\\\\&]/\\\\&/g'`"
-echo "command: \"$rc_local_line\""
-echo "escaped line in /etc/rc.local: \"$escaped_rc_local_line\""
-sudo sed -i "s/^exit 0/$escaped_rc_local_line\nexit 0/g" /etc/rc.local
+echo "Stopping announce server"
+./stop_announce.sh
 
 echo "Executing announce server"
 ./start_announce.sh
